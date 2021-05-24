b57e472f969302ba8fe22b2c5b8dfeaeaf0043a0
Blaxqk <2286746356@qq.com>
2021-05-24 23:05:25 +0800

    每日一题

--- EveryDay/D4_15/Test1.java
+++
@@ -1,8 +0,0 @@
-package D4_15;
-
-public class Test1 {
-    public static void main(String[] args) {
-        String s;
-//        System.out.println("s="+s);
-    }
-}
--- EveryDay/D4_16/Test1.java
+++
@@ -1,8 +0,0 @@
-package D4_16;
-
-public class Test1 {
-    public static void main(String[] args) {
-        boolean b=true?false:true==true?false:true;
-        System.out.println(b);
-    }
-}
